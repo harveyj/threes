fce6bf89d83b023f6f648294fcdae0d6f972fb1f
Harvey Jones <harveyj@gmail.com>
2016-10-22 15:21:49 -0400

    Fix random strategy

--- random_strategy.py
+++ random_strategy.py
@@ -7,19 +7,18 @@ class RandomStrategy(threes_strategy.Strategy):
     def __init__(self, board):
         super(RandomStrategy, self).__init__(board)
 
-    ## DOESN'T WORK YET
     def get_next_move(self):
-        if not can_move(self.board.cell_store):
+        if not board_locked(self.board.cell_store):
             print "BLOCKED"
             print self.board
             return None
         return random.randint(0,3)
 
 
-def can_move(cells):
+def board_locked(cells):
     b = board.Board()
     l_copy = b.transform(copy.deepcopy(cells), LEFT)
     r_copy = b.transform(copy.deepcopy(cells), RIGHT)
     u_copy = b.transform(copy.deepcopy(cells), UP)
     d_copy = b.transform(copy.deepcopy(cells), DOWN)
-    return b == l_copy == r_copy == u_copy == d_copy
+    return not cells == l_copy == r_copy == u_copy == d_copy
